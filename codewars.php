303b1aace83d15125b243f80358dbd6200a5031b
Jhonny <gonzodzen@mail.ru>
2023-06-20 17:13:41 +0300

    Classic Hello World from codewars

---
+++ codewars.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?='Codewars'?></title>
+</head>
+<body>
+    <?php
+    class Solution
+    {
+        static function main() {
+            echo 'Hello World!';
+        }
+    }
+    Solution::main()
+    ?>
+</body>
+</html>
