30144617894530383b09594cd94b7ece4d9bf101
Jhonny <gonzodzen@mail.ru>
2023-06-21 08:51:29 +0300

    isset

--- practice.php
+++ practice.php
@@ -3,11 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?='Codewars'?></title>
+    <title>Practice</title>
 </head>
 <body>
     <?php
-    
+    $a=5;
+    if(isset($a)){
+        echo 'true';
+    } else{
+        echo 'false';
+    }
     ?>
 </body>
 </html>
